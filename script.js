ed4243895ae8c2f1880320116af56c0abe57748b
Joshua <joshua.bogart.ndt@gmail.com>
2020-05-25 12:47:04 -0500

    added arrays

---
+++ script.js
@@ -0,0 +1,80 @@
+//special characters
+var specialCharacters = [
+    '!',
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*',
+    '(',
+    ')',
+    '_',
+    '-',
+    '+',
+    '=',
+];
+
+//uppercase letters
+var UpperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    's',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+];
+
+//lowercase letters
+var lowerCasedLetters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
+
+//numbers
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
